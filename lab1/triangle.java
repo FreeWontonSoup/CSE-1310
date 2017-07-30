314d17f42b754988d9cae94731ffd3668e22d772
Brandon <theauditorexploits@gmail.com>
2017-07-30 12:24:23 -0500

    Add files via upload

---
+++ lab1/triangle.java
@@ -0,0 +1,10 @@
+public class triangle
+{
+  public static void main(String[] args) 
+  {
+    int base = 3;
+    int height = 5;
+    double area = height * base / 2.0;
+    System.out.println(area);
+  }
+}
